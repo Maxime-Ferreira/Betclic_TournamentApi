550c0906fa8b3e3b65890ef538a85f0edb269855
Maxime <maxime.ferreira.info@gmail.com>
2025-02-05 19:35:48 +0100

    Update build to create jar and create bash file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     kotlin("plugin.serialization") version "1.9.10"
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 group = "com.tournament"
-version = "0.0.1"
+version = "1.0"
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
@@ -31,4 +31,15 @@ dependencies {
     implementation("org.mongodb:mongodb-driver-sync:4.10.0")
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
+}
+
+tasks.register("fatJar", Jar::class) {
+    archiveClassifier.set("all")
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+
+    manifest {
+        attributes["Main-Class"] = "io.ktor.server.netty.EngineMain"
+    }
 }
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CONTAINER_NAME="betclic-test-mongo"
+
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "MongoDB est déjà en cours d'exécution."
+else
+    echo "Lancement de MongoDB sur Docker..."
+    docker run --name $CONTAINER_NAME -p 27017:27017 mongo
+fi
+
+sleep 5
+
+echo "Lancement de l'application..."
+java -jar build/libs/api-all.jar
